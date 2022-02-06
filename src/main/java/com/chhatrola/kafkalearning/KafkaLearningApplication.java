6e8af8a75f4b1b3a1f4e8983cb195391b4b0ad48
niv214 <nirav.chhatrola@gmail.com>
2022-02-06 14:37:31 +0530

    Simple producer to write message with key.

--- src/main/java/com/chhatrola/kafkalearning/KafkaLearningApplication.java
+++ src/main/java/com/chhatrola/kafkalearning/KafkaLearningApplication.java
@@ -1,5 +1,6 @@
 package com.chhatrola.kafkalearning;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,11 @@ public class KafkaLearningApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(KafkaLearningApplication.class, args);
+		new KafkaLearningApplication().execute();
+	}
+
+	public void execute(){
+
 	}
 
 }
