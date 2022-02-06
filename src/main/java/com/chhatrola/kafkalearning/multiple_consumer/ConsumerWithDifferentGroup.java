93ee900bf37c02dae9c226575b946d15f06a327e
niv214 <nirav.chhatrola@gmail.com>
2022-02-06 18:41:54 +0530

    multiple consumer with same group and different group

---
+++ src/main/java/com/chhatrola/kafkalearning/multiple_consumer/ConsumerWithDifferentGroup.java
@@ -0,0 +1,47 @@
+package com.chhatrola.kafkalearning.multiple_consumer;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Properties;
+
+/**
+ * Created by niv214 on 6/2/22.
+ */
+public class ConsumerWithDifferentGroup {
+    KafkaConsumer<String, String> kafkaConsumer;
+
+    ConsumerWithDifferentGroup(){
+        kafkaConsumer = new KafkaConsumer<>(CONSUMER_PROPERTIES());
+    }
+
+    public void consume(){
+        kafkaConsumer.subscribe(Arrays.asList("first_topic"));
+
+        while (true){
+            ConsumerRecords<String, String> records = kafkaConsumer.poll(Duration.ofMillis(10000));
+
+            records.forEach(record -> System.out.println("Key : "+ record.key() + " Value : "+record.value() + "\n" +
+                    "Partition : "+record.partition() +" Offset : "+record.offset()));
+
+        }
+    }
+
+    private static final Properties CONSUMER_PROPERTIES(){
+        Properties properties = new Properties();
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, "group2");   // different group than others
+        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
+        return properties;
+    }
+
+    public static void main(String[] args) {
+        new ConsumerWithDifferentGroup().consume();
+    }
+}
--- src/main/java/com/chhatrola/kafkalearning/multiple_consumer/ProducerRunner.java
+++ src/main/java/com/chhatrola/kafkalearning/multiple_consumer/ProducerRunner.java
@@ -1,15 +1,20 @@
-package com.chhatrola.kafkalearning.basic;
+package com.chhatrola.kafkalearning.multiple_consumer;
 
 import java.util.concurrent.ExecutionException;
 
 /**
  * In this example we have simply created
  * one produer to produce
- * one consumer to consume
+ * multiple consumer to consume
+ * case 1) same group
+ *      partitions will be destributed between consumers.
+ *
+ * case 2) different group
+ *      all group will be assigned with seperate partitions and so that same data in both consumer group.
  *
  * Created by niv214 on 6/2/22.
  */
-public class BasicMain {
+public class ProducerRunner {
 
     private static final String TOPIC = "first_topic";
     private static final String KEY = "Key_";
@@ -17,7 +22,7 @@ public class BasicMain {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         Producer producer = new Producer();
         for(int i=0; i<=10; i++){
-            producer.send(TOPIC, KEY+(i%2), "Hello"+i);
+            producer.send(TOPIC, KEY+(i%3), "Hey there ... "+i);
         }
         System.out.println("------------");
     }
