df553b3dea5bd81b71944d83bb89124c4634e82f
niv214 <nirav.chhatrola@gmail.com>
2022-02-06 11:59:43 +0530

    Kafka understanding doc inside /Docs dir. adn Initial project setup commit

---
+++ src/test/java/com/chhatrola/kafkalearning/KafkaLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chhatrola.kafkalearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
